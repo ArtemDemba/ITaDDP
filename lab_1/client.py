1fff5bea9f6101e1f2ac6948f3bceaff5f59c6e8
Artem Dembovskiy <DVL.45@mail.ru>
2023-03-21 02:20:52 +0300

    Added registration, login, storing users

--- lab_1/client.py
+++ lab_1/client.py
@@ -1,9 +1,25 @@
 import socket
 import threading
 import os
+import json
 
+from server import members
 
 UDP_MAX_SIZE = 65535
+
+
+def print_all_users(username: str) -> None:
+    with open('users.json') as file:
+        users = json.load(file)
+        for username in [user for user in users if user != username]:
+            print(username)
+    # message_with_users = ''
+    # for user in members.values():
+    #     for username, address in members.items():
+    #         if user != address:
+    #             message_with_users += username + str(address[0]) + ' ' + str(address[1]) + '\n'
+    #     print(message_with_users)
+    #     message_with_users = ''
 
 
 def listen(s: socket.socket):
@@ -12,21 +28,68 @@ def listen(s: socket.socket):
         print('\r\r' + msg.decode('ascii') + '\n' + f'you: ', end='')
 
 
-def connect(host: str = '127.0.0.1', port: int = 3000):
+def connect(username: str, host: str = '127.0.0.1', port: int = 3000):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     s.connect((host, port))
 
     threading.Thread(target=listen, args=(s,), daemon=True).start()
 
-    s.send('__join'.encode('ascii'))
+    s.send(f'username {username}'.encode('ascii'))
 
     while True:
         msg = input(f'you: ')
         s.send(msg.encode('ascii'))
 
 
-if __name__ == '__main__':
+def show_registration_page():
+    username = input('Write down your username: ')
+    # with open('users.json') as file:
+    #     users = json.load(file)
+    # users[username] = []
+    # with open('users.json', 'w') as file:
+    #     json.dump(users, file, ensure_ascii=False, indent=4)
+
+    show_all_dialogues_of_user(username)
+
+
+def show_all_dialogues_of_user(username: str):
+    print_all_users(username)
+    connect(username)
+
+    # username = input('Write down username: ')
+    if username == 'q':
+        show_start_page()
+
+
+def show_login_page():
+    username = input('Write down your username: ')
+    with open('users.json') as file:
+        users = json.load(file)
+    if username in users:
+        show_all_dialogues_of_user(username)
+    elif username == 'q':
+        show_start_page()
+    else:
+        raise ValueError("This username doesn't exist(")
+
+
+def show_start_page():
     os.system('clear')
-    print('Welcome to chat!')
-    connect()
+    print('Registration')
+    print('Log in')
+    start_page = input('1 - Registration, 2 - Log in\n')
+    match start_page:
+        case '1':
+            show_registration_page()
+        case '2':
+            show_login_page()
+        case 'q':
+            exit()
+        case _:
+            raise ValueError('Incorrect choice(')
+
+
+if __name__ == '__main__':
+    show_start_page()
+    # connect('')
--- lab_1/server.py
+++ lab_1/server.py
@@ -1,6 +1,10 @@
+import json
 import socket
 
 UDP_MAX_SIZE = 65535
+
+
+members = {'45325 username': ('127.0.0.1', 45325), '41789 username': ('127.0.0.1', 41789)}  # key - username, value - (ip, port)
 
 
 def listen(host: str = '127.0.0.1', port: int = 3000):
@@ -9,25 +13,29 @@ def listen(host: str = '127.0.0.1', port: int = 3000):
     s.bind((host, port))
     print(f'Listening at {host}:{port}')
 
-    members = {}  # key - username, value - port
     while True:
         message, addr = s.recvfrom(UDP_MAX_SIZE)
 
         print(message.decode('ascii'))
 
         if str(addr[1]) + ' username' not in members:
-            members[str(addr[1]) + ' username'] = addr[1]
+            members[str(addr[1]) + ' username'] = addr
 
         if not message:
             continue
 
         client_id = addr[1]
-        if message.decode('ascii') == '__join':
+        print('members: ', members)
+        if message.decode('ascii').startswith('username'):
             print(f'Client {client_id} joined chat')
-            continue
+            with open('users.json') as file:
+                users = json.load(file)
+            with open('users.json', 'w') as file:
+                users[message.decode('ascii').split()[1]] = list(addr)
+                json.dump(users, file, ensure_ascii=False, indent=4)
 
         # msg = f'client{client_id}: {message.decode("ascii")}'
-        s.sendto(message, (addr[0], members[message.decode('ascii') + ' username']))
+        s.sendto(message, members[message.decode('ascii') + ' username'])
 
 
 if __name__ == '__main__':
